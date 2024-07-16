7f37cb0406b3fc1e30a73e5f7feca6ac2199d60f
abaykandari <abaykandari@gmail.com>
2024-07-16 06:39:23 +0530

    deleted phase2redundancy

--- cpmPhaseTwo/src/main/java/com/incture/cpm/Exception/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.incture.cpm.Exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- cpmPhaseTwo/src/main/java/com/incture/cpm/Repo/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.incture.cpm.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.incture.cpm.Entity.Trainer;
-
-public interface TrainerRepository extends JpaRepository<Trainer, String> {
-    
-}
